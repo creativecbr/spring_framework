8b327202c5546a07622f05ebc8cb44d9b1bae97d
Paweł <lesniakcbr@gmail.com>
2020-11-22 15:39:24 +0100

    Repaired references and set class properly. Gateway for connection between user and ads. Port: 8080

--- src/main/java/xvc_studio/pg/edu/pl/PASA/PasaApplication.java
+++ src/main/java/xvc_studio/pg/edu/pl/PASA/PasaApplication.java
@@ -2,6 +2,15 @@ package xvc_studio.pg.edu.pl.PASA;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 
 @SpringBootApplication
@@ -12,4 +21,33 @@ public class PasaApplication {
 		SpringApplication.run(PasaApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder)
+	{
+		return builder.routes()
+				.route("users", r -> r.host("localhost:8080")
+					.and()
+					.path("/users/{username}", "/users")
+					.uri("http://localhost:8081"))
+				.route("ads", r -> r.host("localhost:8080")
+					.and()
+					.path("/ads", "ads/**", "/categories", "/categories/**", "/users/{username}/ads")
+					.uri("http://localhost:8081"))
+				.build();
+	}
+
+	@Bean
+	public CorsWebFilter corsWebFilter() {
+
+		final CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.setAllowedOrigins(Collections.singletonList("*"));
+		corsConfig.setMaxAge(3600L);
+		corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT"));
+		corsConfig.addAllowedHeader("*");
+
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+
+		return new CorsWebFilter(source);
+	}
 }
